4ce2174a61f4b6c474434a52a9bffe91c0d6ad68
Vadym Kotenko <kotenko.vadym1121@vu.cdu.edu.ua>
2021-12-02 14:37:22 +0200

    Створено найпростіший консольний додаток

--- ConsoleApp168/Program.cs
+++ ConsoleApp168/Program.cs
@@ -6,10 +6,7 @@ namespace ConsoleApp168
     {
         static void Main(string[] args)
         {
-            int x = 5;
-            int y = 10;
-            Console.WriteLine("5 + 10 = {0}",x+y);
-            Console.ReadKey();
+            Console.WriteLine("Hello World!");
         }
     }
 }
